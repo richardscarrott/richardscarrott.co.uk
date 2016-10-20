28114ba5049986f153c517f2c72b3e8cbcf7fd7f
Richard Scarrott <richard.scarrott@wipro.com>
2016-10-20 11:30:55 +0100

    fixup! Add functional tests with nightwatchjs

--- server/ghost/config.js
+++ server/ghost/config.js
@@ -1,3 +1,5 @@
+'use strict';
+
 // # Ghost Configuration
 // Setup your Ghost install for various [environments](http://support.ghost.org/config/#about-environments).
 
@@ -5,147 +7,84 @@
 
 const path = require('path');
 
+// Allow ghost config to be toggled via `process.env.GHOST_ENV` rather than the
+// overloaded `NODE_ENV`.
+const env = process.env.GHOST_ENV;
+
+let config;
+
+switch (env) {
+    case 'production':
+        config = {
+            url: 'http://richardscarrott.co.uk',
+            mail: {},
+            database: {
+                client: 'sqlite3',
+                connection: {
+                    filename: path.join(__dirname, '/content/data/ghost.db')
+                },
+                debug: false
+            },
+            server: {
+                host: '0.0.0.0',
+                port: process.env.PORT
+            },
+            paths: {
+                contentPath: path.join(__dirname, '/content/')
+            }
+        };
+    break;
+    case 'test':
+        config = {
+            url: `http://localhost:${process.env.PORT}`,
+            database: {
+                client: 'sqlite3',
+                connection: {
+                    filename: path.join(__dirname, '/content/data/ghost-test.db')
+                },
+                debug: false
+            },
+            server: {
+                host: '127.0.0.1',
+                port: process.env.PORT
+            },
+            paths: {
+                contentPath: path.join(__dirname, '/content/')
+            }
+        };
+    break;
+    default:
+        // development
+        config = {
+            // The url to use when providing links to the site, E.g. in RSS and email.
+            // Change this to your Ghost blog's published URL.
+            url: `http://localhost:${process.env.PORT}`,
+            // #### Database
+            // Ghost supports sqlite3 (default), MySQL & PostgreSQL
+            database: {
+                client: 'sqlite3',
+                connection: {
+                    filename: path.join(__dirname, '/content/data/ghost-dev.db')
+                },
+                debug: false
+            },
+            // #### Server
+            // Can be host & port (default), or socket
+            server: {
+                // Host to be passed to node's `net.Server#listen()`
+                host: '127.0.0.1',
+                // Port to be passed to node's `net.Server#listen()`, for iisnode set this to `process.env.PORT`
+                port: process.env.PORT
+            },
+            // #### Paths
+            // Specify where your content directory lives
+            paths: {
+                contentPath: path.join(__dirname, '/content/')
+            }
+        };
+    break;
+}
+
 module.exports = {
-    // ### Production
-    // When running Ghost in the wild, use the production environment.
-    // Configure your URL and mail settings here
-    production: {
-        url: 'http://richardscarrott.co.uk',
-        mail: {},
-        database: {
-            client: 'sqlite3',
-            connection: {
-                filename: path.join(__dirname, '/content/data/ghost.db')
-            },
-            debug: false
-        },
-        server: {
-            host: '0.0.0.0',
-            port: process.env.PORT
-        },
-        paths: {
-            contentPath: path.join(__dirname, '/content/')
-        }
-    },
-
-    // ### Development **(default)**
-    development: {
-        // The url to use when providing links to the site, E.g. in RSS and email.
-        // Change this to your Ghost blog's published URL.
-        url: `http://localhost:${process.env.PORT}`,
-
-        // Example mail config
-        // Visit http://support.ghost.org/mail for instructions
-        // ```
-        //  mail: {
-        //      transport: 'SMTP',
-        //      options: {
-        //          service: 'Mailgun',
-        //          auth: {
-        //              user: '', // mailgun username
-        //              pass: ''  // mailgun password
-        //          }
-        //      }
-        //  },
-        // ```
-
-        // #### Database
-        // Ghost supports sqlite3 (default), MySQL & PostgreSQL
-        database: {
-            client: 'sqlite3',
-            connection: {
-                filename: path.join(__dirname, '/content/data/ghost-dev.db')
-            },
-            debug: false
-        },
-        // #### Server
-        // Can be host & port (default), or socket
-        server: {
-            // Host to be passed to node's `net.Server#listen()`
-            host: '127.0.0.1',
-            // Port to be passed to node's `net.Server#listen()`, for iisnode set this to `process.env.PORT`
-            port: process.env.PORT
-        },
-        // #### Paths
-        // Specify where your content directory lives
-        paths: {
-            contentPath: path.join(__dirname, '/content/')
-        }
-    },
-
-    // **Developers only need to edit below here**
-
-    // ### Testing
-    // Used when developing Ghost to run tests and check the health of Ghost
-    // Uses a different port number
-    test: {
-        // The url to use when providing links to the site, E.g. in RSS and email.
-        // Change this to your Ghost blog's published URL.
-        url: `http://localhost:${process.env.PORT}`,
-
-        // #### Database
-        // Ghost supports sqlite3 (default), MySQL & PostgreSQL
-        database: {
-            client: 'sqlite3',
-            connection: {
-                filename: path.join(__dirname, '/content/data/ghost-test.db')
-            },
-            debug: false
-        },
-        // #### Server
-        // Can be host & port (default), or socket
-        server: {
-            // Host to be passed to node's `net.Server#listen()`
-            host: '127.0.0.1',
-            // Port to be passed to node's `net.Server#listen()`, for iisnode set this to `process.env.PORT`
-            port: process.env.PORT
-        },
-        // #### Paths
-        // Specify where your content directory lives
-        paths: {
-            contentPath: path.join(__dirname, '/content/')
-        }
-    },
-
-    // ### Testing MySQL
-    // Used by Travis - Automated testing run through GitHub
-    'testing-mysql': {
-        url: 'http://127.0.0.1:2369/blog',
-        database: {
-            client: 'mysql',
-            connection: {
-                host     : '127.0.0.1',
-                user     : 'root',
-                password : '',
-                database : 'ghost_testing',
-                charset  : 'utf8'
-            }
-        },
-        server: {
-            host: '127.0.0.1',
-            port: '2369'
-        },
-        logging: false
-    },
-
-    // ### Testing pg
-    // Used by Travis - Automated testing run through GitHub
-    'testing-pg': {
-        url: 'http://127.0.0.1:2369/blog',
-        database: {
-            client: 'pg',
-            connection: {
-                host     : '127.0.0.1',
-                user     : 'postgres',
-                password : '',
-                database : 'ghost_testing',
-                charset  : 'utf8'
-            }
-        },
-        server: {
-            host: '127.0.0.1',
-            port: '2369'
-        },
-        logging: false
-    }
+    [process.env.NODE_ENV]: config
 };
--- test/functional/nightwatch.conf.js
+++ test/functional/nightwatch.conf.js
@@ -4,56 +4,45 @@ const path = require('path');
 const geckodriver = require('geckodriver');
 
 module.exports = {
-  "src_folders" : ["test/functional"],
-  "output_folder" : "reports",
-
-  "selenium" : {
-    "start_process" : false,
-    "server_path": path.join(__dirname, 'lib/selenium-server-standalone-3.0.0.jar'),
-    "cli_args" : {
-      "webdriver.chrome.driver" : "/usr/local/bin/chromedriver",
-      "webdriver.gecko.driver" : "/usr/local/bin/geckodriver",
-      // "webdriver.gecko.driver" : geckodriver.path,
+    src_folders: ['test/functional/tests'],
+    output_folder: 'test/functional/reports',
+    selenium: {
+        start_process: !process.env.TRAVIS,
+        server_path: path.join(__dirname, 'lib/selenium-server-standalone-3.0.0.jar'),
+        cli_args: {
+            'webdriver.chromedriver': '/usr/local/bin/chromedriver',
+            'webdriver.geckodriver': '/usr/local/bin/geckodriver'
+        }
+    },
+    test_settings: {
+        default: {
+            launch_url: 'http://localhost:6060',
+            desiredCapabilities: {
+                browserName: 'chrome'
+            },
+            globals: {
+                waitForConditionTimeout: 2000
+            }
+        },
+        saucelabs: {
+            selenium_port: 80,
+            selenium_host: 'ondemand.saucelabs.com',
+            username: process.env.SAUCE_USERNAME,
+            access_key: process.env.SAUCE_ACCESS_KEY,
+            desiredCapabilities: {
+                build: 'nightwatch-sauce-labs',
+                'tunnel-identifier': process.env.TRAVIS_JOB_NUMBER,
+                browserName: 'chrome',
+                platform: 'OS X 10.11',
+                version: '53.0',
+                browserName: 'chrome'
+            }
+        },
+        firefox: {
+            desiredCapabilities: {
+                browserName: 'firefox',
+                marionette: true
+            }
+        }
     }
-  },
-
-  "test_settings" : {
-    "default": {
-      "launch_url": "http://localhost:6060",
-      "desiredCapabilities": {
-        "browserName": "firefox",
-        "marionette": true
-      },
-      "globals": {
-        "waitForConditionTimeout": 2000
-      }
-    },
-
-    "saucelabs" : {
-      'selenium_port': 80,
-      'selenium_host': 'ondemand.saucelabs.com',
-      "username" : process.env.SAUCE_USERNAME,
-      "access_key" : process.env.SAUCE_ACCESS_KEY,
-      "desiredCapabilities": {
-        'build': 'nightwatch-sauce-labs',
-        'tunnel-identifier': process.env.TRAVIS_JOB_NUMBER,
-        browserName: 'chrome',
-        platform: 'OS X 10.11',
-        version: '53.0',
-        browserName: 'chrome'
-      }
-    },
-
-    "chrome" : {
-      "desiredCapabilities": {
-        "browserName": "chrome"
-      }
-    },
-
-    "edge" : {
-      "desiredCapabilities": {
-        "browserName": "MicrosoftEdge"
-      }
-    }
-  }
 };
